e1593966314992b981fced27e7bb1de846dba017
deenzelle <denlminecraftspam@gmail.com>
2023-05-02 14:39:18 +1200

    Added comments for each section
    Comments for each section

---
+++ pizza_bot.py
@@ -0,0 +1,19 @@
+#Pizza bot program
+
+#Welcome message with random name
+
+#Create menu for pickup or delivery
+
+#Create pickup function
+
+#Create function so that user can coose number of pizzas required
+
+#Create pizza menu
+
+#Create pizza ordering function
+
+#Create print function to display pizzas ordered, cost and delivery options if required
+
+#Create cancel order function
+
+#Create option for new order or to exit
