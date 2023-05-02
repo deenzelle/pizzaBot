0dd7d9cc9283919f44e8ff315a244186e9ae2c54
deenzelle <denlminecraftspam@gmail.com>
2023-05-02 18:11:48 +1200

    Created v1 of pickup function
    basic instructions and input for name and phone number

---
+++ components/pickup v1.py
@@ -0,0 +1,13 @@
+#component numero tres
+
+print("Please enter the pickup information ")
+print("")
+
+# Customer name
+name = input("Please enter your name ")
+
+# Customer phone number
+phone = input("Please enter your phone number ")
+
+print(name)
+print(phone)
