27300f755ca5b87e34a6303b594ad58b4983830e
deenzelle <denlminecraftspam@gmail.com>
2023-05-02 14:42:43 +1200

    Added Welcome function
    Added welcome function and main function from component 1 v2

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,6 +1,25 @@
 #Pizza bot program
+import random
+#imports ability to import random numbers from the random name list
+from random import randint
+
+#list of random names
+names = ["Bread", "Moana", "Mark", "Phoebe", "Sally", "Michael", "Chaewon", "Haewon", "Hanni", "Joaquin"]
 
 #Welcome message with random name
+def welcome():
+    '''
+    Purpose: To generate a random name from the list and print out 
+    a welcome message
+    Parameter: None
+    Returns: None
+    '''
+
+    num = randint(0,9)
+    name = (names[num])
+    print("*** Welcome to BinkyBong Pizzas ***")
+    print("*** My name is",name, "***")
+    print("*** I will be here to help you order your delicious BinkyBong Pizza")
 
 #Create menu for pickup or delivery
 
@@ -17,3 +36,14 @@
 #Create cancel order function
 
 #Create option for new order or to exit
+
+#Main function
+def main():
+    '''
+    Purpose: To run all functions
+    Parameter: None
+    Returns: None
+    '''
+    welcome()
+
+main()
