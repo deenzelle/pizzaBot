062f571a7c7578c3cf701e45d1797bf7eebaae54
deenzelle <denlminecraftspam@gmail.com>
2023-05-02 17:38:40 +1200

    Created pickup or delivery menu for user
    Finished creating different versions of the p/d menu, refining it in version 5

--- components/pickup_deliveryv3.py
+++ components/pickup_deliveryv3.py
@@ -4,24 +4,23 @@
 # Need to make it so that it only accepts '1' or '2'
 
 print("Is your order being picked up at our store, or would you like it to be delivered?")
-
-print("For delivery enter 1")
-print("For pickup enter 2")
-
-low = 1
-high = 2
+print("For pickup enter 1")
+print("For delivery enter 2")
 
 while True:
     try:
         delivery = int(input("Please enter a number "))
-        if delivery == "1":
-            print("Delivery")
-            break
-    
-        elif delivery == "2":
-            print("Pickup")
-            break
-    
+        if delivery >= 1 and delivery <= 2:
+            if delivery == 1:
+                print("Pickup")
+                break
+
+            elif delivery == 2:
+                print("Delivery")
+                break
+        else:
+            print("The number must be 1 or 2")
+
     except ValueError:
         print("That is not a valid number")
         print("Please enter 1 or 2")
