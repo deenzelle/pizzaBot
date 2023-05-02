4d5ddb08cc5ecc2ddc40b050b57f16aa999d7050
deenzelle <denlminecraftspam@gmail.com>
2023-05-02 18:22:54 +1200

    Added "Not Blank"
    added validation so that entry cannot be blank

---
+++ components/pickup v2.py
@@ -0,0 +1,27 @@
+#component numero tres
+
+#Bug - accepts blank input, allows letters to be inputted as phone *numbers*
+
+print("Please enter the pickup information ")
+
+# Customer name
+valid = False
+while not valid:
+    name = input("Please enter your name ")
+    if name != "":
+        print(name)
+        break
+
+    else:
+        print("Sorry this cannot be blank, please enter your name")
+
+# Customer phone number
+valid = False
+while not valid:
+    phone = input("Please enter your phone number ")
+
+    if phone != "":
+        print(phone)
+        break
+    else:
+        print("Sorry this cannot be blank, please enter your phone number")
