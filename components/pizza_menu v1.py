ce773f832f55f47b0faace94ec9d1642444cb3bb
deenzelle <denlminecraftspam@gmail.com>
2023-05-03 17:56:02 +1200

    Created list of pizza names and pizza prices

---
+++ components/pizza_menu v1.py
@@ -0,0 +1,6 @@
+#component numero 5 v1
+
+pizza_names = ['Margherita', 'Pepperoni', 'Hawaiian', 'Cheese', 'Italian', 'Veggie',
+               'Vegan', 'Chicken Deluxe', 'Mega Meat Lovers', 'Seafood Deluxe', 'Apricot Chicken Deluxe', 'BBQ Chicken Deluxe']
+
+pizza_prices = ['8.50', '8.50', '8.50', '8.50', '8.50', '8.50', '8.50', '13.50', '13.50', '13.50', '13.50', '13.50']
