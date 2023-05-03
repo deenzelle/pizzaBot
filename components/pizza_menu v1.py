5f5ad27d01435f5a58d55e1cce907975d5ea739e
deenzelle <denlminecraftspam@gmail.com>
2023-05-03 18:05:43 +1200

    created a basic pizza menu
    created a basic pizza menu

--- components/pizza_menu v1.py
+++ components/pizza_menu v1.py
@@ -3,4 +3,12 @@
 pizza_names = ['Margherita', 'Pepperoni', 'Hawaiian', 'Cheese', 'Italian', 'Veggie',
                'Vegan', 'Chicken Deluxe', 'Mega Meat Lovers', 'Seafood Deluxe', 'Apricot Chicken Deluxe', 'BBQ Chicken Deluxe']
 
-pizza_prices = ['8.50', '8.50', '8.50', '8.50', '8.50', '8.50', '8.50', '13.50', '13.50', '13.50', '13.50', '13.50']
+pizza_prices = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50, 13.50, 13.50]
+
+number_pizzas = 12
+
+#print("How many Dream pizzas would you like to order today?")
+#num_pizza = int(input)
+
+for count in range (number_pizzas):
+    print(count,pizza_names[count],pizza_prices[count])
