d164223f7940140d61f1c35e5d08d9596e38d6ff
deenzelle <denlminecraftspam@gmail.com>
2023-05-03 12:05:34 +1200

    Updated pizza_bot
    Title class for not blank return
    Integrated pickup function into main code

--- pizza_bot.py
+++ pizza_bot.py
@@ -6,6 +6,19 @@ from random import randint
 #list of random names
 names = ["Bread", "Moana", "Mark", "Phoebe", "Sally", "Michael", "Chaewon", "Haewon", "Hanni", "Joaquin"]
 
+# validates input to check if they are blank
+def not_blank(question):
+    valid = False
+    while not valid:
+        response = input(question)
+        if response != "":
+            return response.title()
+        else:
+            print("This cannot be blank")
+            
+# Creates the customer detail dictionary to have memory of user input
+customer_details = {}
+
 #Welcome message with random name
 def welcome():
     '''
@@ -18,27 +31,28 @@ def welcome():
     num = randint(0,9)
     name = (names[num])
     print("****************************************************************************")
-    print("*** Welcome to BinkyBong Pizzas                                          ***")
+    print("*** Welcome to Dream Pizzas                                          ***")
     print("*** My name is",name, "                                                  ***")
-    print("*** I will be here to help you order your delicious BinkyBong Pizza      ***")
+    print("*** I will be here to help you order your delicious Dream Pizza      ***")
     print("****************************************************************************")
 
 #Create menu for pickup or delivery
-def pickup():
+def order_type():
     print("Is your order being picked up at our store, or would you like it to be delivered?")
     print("For pickup enter 1")
     print("For delivery enter 2")
 
     while True:
         try:
-            delivery = int(input("Please enter a number "))
+            delivery = int(input("Please enter a number: "))
             if delivery >= 1 and delivery <= 2:
                 if delivery == 1:
-                    print("Pickup")
+                    print("You have selected to have your pizza picked up!")
+                    pickup()
                     break
 
                 elif delivery == 2:
-                    print("Delivery")
+                    print("You have selected to have your pizza delivered!")
                     break
             else:
                 print("The number must be 1 or 2")
@@ -47,9 +61,21 @@ def pickup():
             print("That is not a valid number")
             print("Please enter 1 or 2")
 
-#Create pickup function
 
-#Create function so that user can coose number of pizzas required
+# Grabbing the user's name and phone number for pickup information
+# instructions for asking name
+def pickup():
+    question = ("Please enter your name: ")
+    customer_details['name'] = not_blank(question )
+    # print (customer_details['name'])
+
+    # instructions for asking for user's phone number
+    question = ("Please enter your phone number: ")
+    customer_details['phone'] = not_blank(question )
+    # print (customer_details['phone'])
+    print(customer_details)
+
+#Create function so that user can choose number of pizzas required
 
 #Create pizza menu
 
@@ -69,7 +95,7 @@ def main():
     Returns: None
     '''
     welcome()
-    pickup()
+    order_type()
 
 
 main()
