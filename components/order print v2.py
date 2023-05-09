0a4c30fe075e5b823d35fd891bfb2a8af72c3cf8
deenzelle <denlminecraftspam@gmail.com>
2023-05-10 10:36:54 +1200

    print out Customer details
    Added code to print customer details, order printout v2
    
    (lesson 22 done)

--- components/order print v2.py
+++ components/order print v2.py
@@ -6,6 +6,10 @@ order_cost = [8.50, 8.50, 8.50, 13.50]
 
 cust_details = {'name': 'Denz', 'phone': '0211690403', 'house': '69', 'street': 'Tokki Drive', 'suburb': 'Flat Bush'}
 
+#print("\n",cust_details['name'], "\n",cust_details['phone'], "\n",cust_details['house'], "\n",cust_details['street'], "\n",cust_details['suburb'],)
+
+print("\nCustomer Name: {} \nCustomer Phone: {} \nCustomer House Number: {} \nCustomer Street Name: {} \nCustomer Suburb: {}".format(cust_details['name'],cust_details['phone'],cust_details['house'],cust_details['street'],cust_details['suburb'],))
+
 count=0
 for item in order_list:
     print("Ordered: {}  Cost: ${:.2f}" .format(item, order_cost[count]))
